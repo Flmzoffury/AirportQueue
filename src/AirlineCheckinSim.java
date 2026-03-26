8c2cdb3cf846e77f32c7f0615036c1abf136bf41
samsi <samsippius@gmail.com>
2026-03-26 19:10:15 -0400

    Fixed Airport Queue Sim

---
+++ src/AirlineCheckinSim.java
@@ -0,0 +1,122 @@
+/** Simulate the check-in process of an airline.
+ */
+import java.util.Scanner;
+
+public class AirlineCheckinSim {
+// Data Fields
+    /**
+     * Queue of frequent flyers.
+     */
+    private static PassengerQueue frequentFlyerQueue =
+            new PassengerQueue("Frequent Flyer");
+    /**
+     * Queue of regular passengers.
+     */
+    private static PassengerQueue regularPassengerQueue =
+            new PassengerQueue("Regular Passenger");
+    /**
+     * Maximum number of frequent flyers to be served
+     * before a regular passenger gets served.
+     */
+    private static int frequentFlyerMax;
+    /**
+     * Maximum time to service a passenger.
+     */
+    private static int maxProcessingTime;
+    /**
+     * Total simulated time.
+     */
+    private static int totalTime;
+    /**
+     * If set true, print additional output.
+     */
+    private static boolean showAll;
+    /**
+     * Simulated clock.
+     */
+    private int clock = 0;
+    /**
+     * Time that the agent will be done with the current passenger.
+     */
+    private int timeDone;
+    /**
+     * Number of frequent flyers served since the
+     * last regular passenger was served.
+     */
+    private int frequentFlyersSinceRP;
+
+    /**
+     * Main method.
+     *
+     * @param args Not used
+     */
+    public static void main(String[] args) {
+        AirlineCheckinSim sim = new AirlineCheckinSim();
+        sim.enterData();
+        sim.runSimulation();
+        sim.showStats();
+        System.exit(0);
+    }
+
+    private void runSimulation() {
+        for (clock = 0; clock < totalTime; clock++) {
+            frequentFlyerQueue.checkNewArrival(clock, showAll);
+            regularPassengerQueue.checkNewArrival(clock, showAll);
+            if (clock >= timeDone) {
+                startServe();
+            }
+        }
+    }
+
+    private void startServe() {
+        if (!frequentFlyerQueue.isEmpty()
+                && ((frequentFlyersSinceRP <= frequentFlyerMax)
+                || regularPassengerQueue.isEmpty())) {
+// Serve the next frequent flyer.
+            frequentFlyersSinceRP++;
+            timeDone = frequentFlyerQueue.update(clock, showAll);
+        } else if (!regularPassengerQueue.isEmpty()) {
+// Serve the next regular passenger.
+            frequentFlyersSinceRP = 0;
+            timeDone = regularPassengerQueue.update(clock, showAll);
+        } else if (showAll) {
+            System.out.println("Time is " + clock
+                    + " server is idle");
+        }
+    }
+
+    /** Method to show the statistics. */
+    private void showStats() {
+        System.out.println
+                ("\nThe number of regular passengers served was "
+                        + regularPassengerQueue.getNumServed());
+        double averageWaitingTime =
+                (double) regularPassengerQueue.getTotalWait()
+                        / (double) regularPassengerQueue.getNumServed();
+        System.out.println(" with an average waiting time of "
+                + averageWaitingTime);
+        System.out.println("The number of frequent flyers served was "
+                + frequentFlyerQueue.getNumServed());
+        averageWaitingTime =
+                (double) frequentFlyerQueue.getTotalWait()
+                        / (double) frequentFlyerQueue.getNumServed();
+        System.out.println(" with an average waiting time of "
+                + averageWaitingTime);
+        System.out.println("Passengers in frequent flyer queue: "
+                + frequentFlyerQueue.size());
+        System.out.println("Passengers in regular passenger queue: "
+                + regularPassengerQueue.size());
+    }
+
+    private static void enterData()
+    {
+        //Scanner inputReader = new Scanner(System.in);
+        showAll = false;
+        totalTime = 20;
+        maxProcessingTime = 50;
+        frequentFlyerMax = 1;
+        frequentFlyerQueue.setArrivalRate(2);
+        regularPassengerQueue.setArrivalRate(2);
+
+    }
+}
---
+++ src/Passenger.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+/** A class to represent a passenger. */
+public class Passenger {
+// Data Fields
+    /**
+     * The ID number for this passenger.
+     */
+    private int passengerId;
+    /**
+     * The time needed to process this passenger.
+     */
+    private int processingTime;
+    /**
+     * The time this passenger arrives.
+     */
+    private int arrivalTime;
+    /**
+     * The maximum time to process a passenger.
+     */
+    private static int maxProcessingTime;
+    /**
+     * The sequence number for passengers.
+     */
+    private static int idNum = 0;
+    /**
+     * Randomizer
+     */
+    private Random randGen;
+
+    /**
+     * Create a new passenger.
+     *
+     * @param arrivalTime The time this passenger arrives
+     */
+    public Passenger(int arrivalTime) {
+        randGen = new Random();
+        this.arrivalTime = arrivalTime;
+        //processingTime = 1 + randGen.nextInt(maxProcessingTime);
+        passengerId = idNum++;
+    }
+
+    /**
+     * Get the arrival time.
+     *
+     * @return The arrival time
+     */
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    /**
+     * Get the processing time.
+     *
+     * @return The processing time
+     */
+    public int getProcessingTime() {
+        return processingTime;
+    }
+
+    /**
+     * Get the passenger ID.
+     *
+     * @return The passenger ID
+     */
+    public int getId() {
+        return passengerId;
+    }
+
+    /**
+     * Set the maximum processing time
+     *
+     * @param maxProcessTime The new value
+     */
+    public void setMaxProcessingTime(int maxProcessTime) {
+        maxProcessingTime = maxProcessTime;
+        processingTime = 1 + randGen.nextInt(maxProcessingTime);
+    }
+}
